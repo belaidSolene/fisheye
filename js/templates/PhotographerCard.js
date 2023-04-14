445d3fddd978f5ac8219c850a9f7bb6dcad97893
Belaid Solene <solene.belaid@greta-cfa-aquitaine.academy>
2023-04-14 13:19:14 +0200

    css photographer-section

--- js/templates/PhotographerCard.js
+++ js/templates/PhotographerCard.js
@@ -9,15 +9,15 @@ class PhotographerCard {
 
         const photographerCard = `
             <a href="#">
-                <img class="photographerCard__pp" 
+                <img class="photographer-card__pp" 
                     src="${this._photographer.portrait}" 
                     alt="">
                 <h2>${this._photographer.name}</h2>
             </a>
 
-            <p class="photographerCard__details photographerCard__details--localisation">${this._photographer.localisation}</p>
-            <p class="photographerCard__details photographerCard__details--tagline">${this._photographer.tagline}</p>
-            <p class="photographerCard__details photographerCard__details--price">${this._photographer.price}</p>
+            <p class="photographerCard__details photographer-card__details--localisation">${this._photographer.localisation}</p>
+            <p class="photographerCard__details photographer-card__details--tagline">${this._photographer.tagline}</p>
+            <p class="photographerCard__details photographer-card__details--price">${this._photographer.price}</p>
         `
 
         $wrapper.innerHTML = photographerCard;
