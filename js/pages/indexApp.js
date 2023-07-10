9ab2e4155167b569a2b341f68f2bcfaf5027a078
Belaid Solene <solene.belaid@greta-cfa-aquitaine.academy>
2023-07-10 08:54:41 +0200

    testing files path

--- js/pages/indexApp.js
+++ js/pages/indexApp.js
@@ -2,7 +2,9 @@ class IndexApp extends App {
     constructor() {
         super();
         this.$photographersWrapper = document.querySelector('.photographer-section')
-        this.photographersApi = new PhotographersApi(window.location.href + '/data/photographers-data.json')
+        console.log(window.location.href);
+        console.log(window.location.href + 'data/photographers-data.json');
+        this.photographersApi = new PhotographersApi(window.location.href + 'data/photographers-data.json')
     }
 
     async main() {
